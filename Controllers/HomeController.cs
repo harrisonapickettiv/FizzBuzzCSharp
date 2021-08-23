9cc2007a640ec09a6ca1e1950a66e3f850a92aaf
Harrison A. Pickett, IV <38798125+harrisonapickettiv@users.noreply.github.com>
2021-08-23 17:17:01 -0500

    Build FizzBuzz UI with C# MVC

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,29 +9,47 @@ using FizzBuzzMVC.Models;
 
 namespace FizzBuzzMVC.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _logger = logger;
     }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpGet]
+    public IActionResult FBPage()
+    {
+      FizzBuzz model = new();
+
+      model.FizzValue = 3;
+      model.BuzzValue = 5;
+
+      return View(model);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult FBPage(FizzBuzz model)
+    {
+      return View(model);
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
